bfd64bdb2f758ecb646b17dd8e684f183c285459
Ralenac <t.ralenac@gmail.com>
2021-11-15 16:27:21 -0500

    Add validations for name, price, quantity, category of the product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,38 @@
+require 'rails_helper'
+
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    category = Category.new(name: "Furniture")
+    product = Product.new(name: "Red sofa", price: 5460, quantity: 20, category: category)
+
+    it 'is valid with valid attributes' do
+      expect(product).to be_valid
+    end
+
+    it "is not valid without a name" do
+      product.name = nil
+      expect(product).to_not be_valid
+      expect(product.errors.messages[:name]).to eq ["can't be blank"]
+    end
+
+    it "is not valid without a price" do
+      product.price_cents = nil
+      expect(product).to_not be_valid
+      expect(product.errors.messages[:price]).to eq ["is not a number", "can't be blank"]
+    end
+
+    it "is not valid without a quantity" do
+      product.quantity = nil
+      expect(product).to_not be_valid
+      expect(product.errors.messages[:quantity]).to eq ["can't be blank"]
+    end
+
+    it "is not valid without a category" do
+      product.category = nil
+      expect(product).to_not be_valid
+      expect(product.errors.messages[:category]).to eq ["can't be blank"]
+    end
+  
+
   end
 end
